92b52009761bbe474703db935927fe9e1f732f5a
chamindu_a.15 <chamindua15@gmail.com>
2024-03-02 19:58:54 +0530

    Email Validation

--- src/main/java/com/craftbay/crafts/controller/AuthController.java
+++ src/main/java/com/craftbay/crafts/controller/AuthController.java
@@ -13,7 +13,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -54,8 +60,23 @@ public class AuthController {
     }
 
     @PostMapping("/save")
-    public String saveUser(@RequestBody RegisterRequestDto request){
+    public String saveUser(@Valid @RequestBody RegisterRequestDto request){
         String id = userService.registerUser(request);
         return id;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
