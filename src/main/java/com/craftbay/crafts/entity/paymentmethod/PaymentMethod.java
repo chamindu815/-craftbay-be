b35e8872b757663d91d30ae3efc9526e8c6e6213
chamindu_a.15 <chamindua15@gmail.com>
2024-03-10 23:33:12 +0530

    Payment Method & Product Generated Value

--- src/main/java/com/craftbay/crafts/entity/paymentmethod/PaymentMethod.java
+++ src/main/java/com/craftbay/crafts/entity/paymentmethod/PaymentMethod.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 @Builder
 public class PaymentMethod {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @OneToOne(cascade = CascadeType.ALL)
--- src/main/java/com/craftbay/crafts/entity/product/Product.java
+++ src/main/java/com/craftbay/crafts/entity/product/Product.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
